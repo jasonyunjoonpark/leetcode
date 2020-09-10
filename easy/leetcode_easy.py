d7c06b6587aa74e61d6b45d3ba5c330b6f4a2076
Jason Park <34497076+jasonyunjoonpark@users.noreply.github.com>
2020-09-10 17:54:26 -0500

    Update leetcode_easy.py

--- easy/leetcode_easy.py
+++ easy/leetcode_easy.py
@@ -18,3 +18,47 @@ class Solution(object):
         dict[num] = i
       
       i += 1
+
+      
+##################################
+##    002 Add Two Numbers       ##
+##################################      
+class ListNode(object):
+    def __init__(self, data, next_node = None):
+        self.data = data
+        self.next_node = next_node
+                       
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
